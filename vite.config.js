93f5b044f9e41320421a92757c3e195df9ae2331
Isidro <i.martin@pristok.com>
2025-03-01 04:56:50 +0100

    Fix Vercel routing issue

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/lavalensiana/",
+  base: "/",
   plugins: [react()],
   build: {
+    outDir: "dist",
     rollupOptions: {
       input: {
         main: "index.html",
